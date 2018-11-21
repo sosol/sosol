sosol/sosol

commit 6d1ef15039ab6aea9588ec79936009de81de4e41

cleaned up and documented

app/models/cts_identifier.rb
@@ -10,52 +10,104 @@ class CTSIdentifier < Identifier
   TEMPORARY_TITLE = 'New Transcription'
   NS_TEI = "http://www.tei-c.org/ns/1.0"
 
-  # responds to an interface request to retitle the file
-  # by updating the label for it in its related text inventory
-  def update_title new_title,lang='eng'
-    self.related_inventory.update_version_label(self.urn_attribute, title, lang)
-  end
-  
-  def titleize
-    begin
-      # if we don't have a publication associated with identifier yet, we're most likely
-      # looking for the title for the parent 'publication' which probably should be the work title
-      # and not the version-specific label
-      if nil == self.publication
-        title = CTS::CTSLib.workTitleForUrn(self.inventory,self.urn_attribute)
+
+  ###################################
+  # Public Class Method Overrides
+  ###################################
+
+  # @Overrides Identifier#new_from_supplied
+  # in additionl to pulling identifier from supplied content
+  # it updates the identifier in the content to replace
+  # a work urn with the new version urn identifier
+  def self.new_from_supplied(publication,agent,content,comment)
+    new_identifier = self.new(:name => self.identifier_from_content(agent,content))
+    Identifier.transaction do
+      publication.lock!
+      if publication.identifiers.select{|i| i.class == self}.length > 0
+        return nil
       else
-        title = CTS::CTSLib.versionTitleForUrn(self.inventory,self.urn_attribute)
+        new_identifier.publication = publication
+        new_identifier.save!
       end
-    rescue Exception => e
-      Rails.logger.error("Error retrieving title from #{self.inventory} at #{self.urn_attribute}: #{e.class.to_s}, #{e.to_s}")
     end
-    if (title.nil? || title == '')
-      title = self.urn_attribute
-    end
-  
-    return title
+    urn = new_identifier.urn_attribute
+    urnObj = CTS::CTSLib.urnObj(urn)
+    workUrn = "urn:cts:" + urnObj.getTextGroup(true) + "." + urnObj.getWork(false)
+    # TODO we should really only do this explicitly in the idno header
+    content.gsub(/\b#{workUrn}\b/,temp_id.urn_attribute)
+    new_identifier.set_content(body, :comment => comment, :actor => (publication.owner.class == User) ? publication.owner.jgit_actor : publication.creator.jgit_actor)
+    template_init = new_identifier_id.add_change_desc(comment)
+    new_identifier.set_xml_content(template_init, :comment => 'Initializing Content')
+    return new_identifier
   end
 
+  # @Overrides Identifier#identifier_from_content
+  # to parse a work urn from supplied content
+  def self.identifier_from_content(agent,content)
+    xml = REXML::Document.new(content).root
+    urn = REXML::XPath.first(xml,'/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:idno[@type="urn:cts"]',{"tei" => NS_TEI})
+    text = REXML::XPath.first(xml,'/tei:TEI/tei:text/tei:body/tei:div',{"tei" => NS_TEI})
+    unless urn.nil?
+      urn = urn.text
+    end
+    unless text.nil?
+      lang = text.attributes['xml:lang']
+      pubtype = text.attributes['type']
+    end
+    unless (urn && lang && pubtype)
+      raise Exception.new("Unable to parse information for new URN identifier")
+    end
+    begin
+      urnObj = CTS::CTSLib.urnObj(urn)
+    rescue
+      raise Exception.new("Invalid URN identifier #{urn}")
+    end
+    # we must have at least a work
+    work = urnObj.getWork(false)
+    if (work.nil? || work == '')
+      raise Exception.new("Missing work identifier in #{urn}")
+    end
+    version = urnObj.getVersion(false)
+    if (version)
+      raise Exception.new("Creating a new version from a version URN is not yet supported")
+    end
+    self::next_temporary_identifier(self::TEMPORARY_COLLECTION,urn,pubtype,lang)
+  end
+
+  ###########################################
+  # CTS Identifier Specific Class Methods
+  ###########################################
+
+  # Different signature for this method than in Identifier#new_from_template
+  # because we need the inventory,pubtype and lang in addition to the publication
+  # in order to construct a new identifier
+  # This may all change when we move to CapiTaiNs and CTS5
+  # - *Args* :
+  #   - +publication+ -> parent publication
+  #   - +inventory+-> cts inventory
+  #   - +urn+ -> base work urn
+  #   - +pubtype+ -> edition or translation
+  #   - +lang+ -> language of the edition or translation
+  # - *Returns* :
+  #   - newly constructed identifier
   def self.new_from_template(publication,inventory,urn,pubtype,lang)
     temp_id = self.new(:name => self.next_temporary_identifier(inventory,urn,pubtype,lang))
-    temp_id.publication = publication 
+    temp_id.publication = publication
     temp_id.save!
     initial_content = temp_id.file_template
     temp_id.set_content(initial_content, :comment => 'Created from SoSOL template', :actor => (publication.owner.class == User) ? publication.owner.jgit_actor : publication.creator.jgit_actor)
     return temp_id
   end
-  
-  def self.new_from_supplied(publication,inventory,urn,pubtype,lang,initial_content)
-    # TODO - we shouldn't really supply pubtype and lang in param - instead parse it from the content
-    temp_id = self.new(:name => self.next_temporary_identifier(inventory,urn,pubtype,lang))
-    temp_id.publication = publication 
-    temp_id.save!
-    ## replace work urn with version 
-    initial_content = initial_content.gsub!(/#{urn}/,temp_id.urn_attribute)
-    temp_id.set_content(initial_content, :comment => 'Created from Supplied content', :actor => (publication.owner.class == User) ? publication.owner.jgit_actor : publication.creator.jgit_actor)
-    return temp_id
-  end
-  
+
+  # Instantiate an existing version of a text from the local inventory
+  # This may all change when we move to CapiTaiNs and CTS5
+  # - *Args* :
+  #   - +publication+ -> parent publication
+  #   - +inventory+-> cts inventory
+  #   - +urn+ -> base work urn
+  #   - +pubtype+ -> edition or translation
+  # - *Returns* :
+  #   - newly constructed identifier
   def self.new_from_inventory(publication,inventory,urn,pubtype)
     document_path = inventory + "/" + CTS::CTSLib.pathForUrn(urn,pubtype)
     temp_id = self.new(:name => document_path)
@@ -77,10 +129,13 @@ class CTSIdentifier < Identifier
     #end
   end
   
-  def self.inventories_hash
-    return CTS::CTSLib.getInventoriesHash()
-  end
-
+  # Different signature for this method than in Identifier#next_temporary_identifier
+  # because we need the inventory,pubtype and lang in addition to the publication
+  # - *Args* :
+  #   - +collection+ -> cts inventory
+  #   - +template+-> urn template
+  #   - +pubtype+ -> edition or translation
+  #   - +lang+ -> language code of the edition or translation
   def self.next_temporary_identifier(collection,template,pubtype,lang)
     urnObj = CTS::CTSLib.urnObj(template)
     year = Time.now.year
@@ -106,28 +161,123 @@ class CTSIdentifier < Identifier
     # TODO add exemplar (version) component
     return "#{document_path}#{document_number.to_s}"
   end
-  
-  def self.collection_names
-    unless defined? @collection_names
-      @collection_names = self.identifier_hash.keys
-    end
-    return @collection_names
-  end
-  
-  def self.collection_names_hash
-    self.collection_names
-    
-    unless defined? @collection_names_hash
-      @collection_names_hash = {TEMPORARY_COLLECTION => TEMPORARY_COLLECTION}
-      @collection_names.each do |collection_name|
-        human_name = collection_name.tr('_',' ')
-        @collection_names_hash[collection_name] = human_name
+
+  ###########################################
+  # Public Instance Method Overrides
+  ###########################################
+
+  # @overrides Identifier#titleize
+  # To set the title from the CTS inventory information
+  def titleize
+    begin
+      # if we don't have a publication associated with identifier yet, we're most likely
+      # looking for the title for the parent 'publication' which probably should be the work title
+      # and not the version-specific label
+      if nil == self.publication
+        title = CTS::CTSLib.workTitleForUrn(self.inventory,self.urn_attribute)
+      else
+        title = CTS::CTSLib.versionTitleForUrn(self.inventory,self.urn_attribute)
       end
+    rescue Exception => e
+      Rails.logger.error("Error retrieving title from #{self.inventory} at #{self.urn_attribute}: #{e.class.to_s}, #{e.to_s}")
     end
-    
-    return @collection_names_hash
+    if (title.nil? || title == '')
+      title = self.urn_attribute
+    end
+
+    return title
   end
-  
+
+  def id_attribute
+     # TODO figure out best way to handle urn as id attribute (: not allowed)
+     return (IDENTIFIER_PREFIX + self.to_urn_components.join("_")).gsub!(/:/,'_')
+  end
+
+  def n_attribute
+    return id_attribute
+  end
+
+  def xml_title_text
+    # TODO lookup title
+    self.urn_attribute
+  end
+
+  def to_path
+    path_components = [ self.class::PATH_PREFIX ]
+    temp_components = self.to_components
+     # should give us, e.g.
+    # [0] collection = e.g. perseus
+    # [1] namespace - e.g. greekLang
+    # [2] work - e.g. tlg0012.tlg001
+    # [3] edition or translation
+    # [4] perseus-grc1 - edition + examplar
+    # [5] 1.1 - passage
+    cts_inv = temp_components[0]
+    cts_ns = temp_components[1]
+    cts_urn = temp_components[2] + "." + temp_components[4]
+    cts_passage = temp_components[5]
+
+    cts_textgroup,cts_work,cts_edition,cts_exemplar, =
+      cts_urn.split('.',4).collect {|x| x.tr(',/','-_')}
+
+    # e.g. tlg0012.tlg001.perseus-grc1.1.1.xml
+    cts_xml_path_components = []
+    cts_xml_path_components << cts_textgroup
+    unless cts_work.nil?
+      cts_xml_path_components << cts_work
+    end
+    cts_xml_path_components << cts_edition
+    unless cts_exemplar.nil?
+      cts_xml_path_components << cts_exemplar
+    end
+    unless cts_passage.nil?
+      cts_xml_path_components << cts_passage
+    end
+    cts_xml_path_components << 'xml'
+    cts_xml_path = cts_xml_path_components.join('.')
+
+    path_components << cts_inv
+    path_components << cts_ns
+    path_components << cts_textgroup
+    unless cts_work.nil?
+      path_components << cts_work
+    end
+    path_components << cts_xml_path
+
+    # e.g. CTS_XML_PASSAGES/perseus/greekLang/tlg0012/tlg001/tlg0012.tlg001.perseus-grc1.1.1.xml
+    return File.join(path_components)
+  end
+
+   ## get a link to the catalog for this identifier
+  def get_catalog_link
+    #return "http://catalog.perseus.tufts.edu/perseus.org/xc/search/" + self.urn_attribute
+    return ''
+  end
+
+  def download_file_name
+    urnObj = CTS::CTSLib.urnObj(self.urn_attribute)
+    file = urnObj.getTextGroup(false) + "." + urnObj.getWork(false) + "." + urnObj.getVersion(false)
+    begin
+      passage = urnObj.getPassage(100)
+      file = passage ? file + "." + passage : file
+    rescue
+    end
+    file = file + ".xml"
+    file
+  end
+
+
+  ###########################################
+  # CTS Identifier specific Instance Methods
+  ###########################################
+
+  # responds to an interface request to retitle the file
+  # by updating the label for it in its related text inventory
+  def update_title new_title,lang='eng'
+    self.related_inventory.update_version_label(self.urn_attribute, title, lang)
+  end
+
+  # retrieve the language of the identifier content
   def lang
     # we want to cache this call because (1) it's not likely to change often 
     # and (2) as we may call it in a request that subsequently retrieves the
@@ -141,39 +291,20 @@ class CTSIdentifier < Identifier
     end
   end
   
-  def reprinted_in
-    # checking for reprints is not part of the CTS model
-    nil
-  end
-  
-  def is_reprinted?
-    return reprinted_in.nil? ? false : true
-  end
-  
+  # Return the identifier formatted as full urn for inclusion in an XML attribute
   def urn_attribute
      return IDENTIFIER_PREFIX + self.to_urn_components.join(":")
   end
-  
+
+  # Return the work level urn for this identifer
   def work_urn
     urn_obj = CTS::CTSLib.urnObj(self.urn_attribute)
     work_urn = IDENTIFIER_PREFIX + urn_obj.getTextGroup() + '.' + urn_obj.getWork(false)
     return work_urn
   end
   
-  def id_attribute
-     # TODO figure out best way to handle urn as id attribute (: not allowed)
-     return (IDENTIFIER_PREFIX + self.to_urn_components.join("_")).gsub!(/:/,'_')
-  end
-  
-  def n_attribute
-    return id_attribute
-  end
-  
-  def xml_title_text
-    # TODO lookup title
-    self.urn_attribute
-  end
-  
+
+  # Return the inventory name
   def inventory
     return self.to_components[0]
   end
@@ -225,7 +356,8 @@ class CTSIdentifier < Identifier
     end
     return inv    
   end
-  
+
+  # check to see if any citations have been extracted in the publication
   def has_related_citations
       cites = self.publication.identifiers.select{|i| (i.class == CitationCTSIdentifier)}
       return cites.size() > 0
@@ -256,10 +388,13 @@ class CTSIdentifier < Identifier
     Rails.cache.delete("#{self.publication.cache_key}/#{self.id}/validreffs")
   end
 
-  def related_inventory 
+
+  # return the related CTSInventory identifier
+  def related_inventory
     self.publication.identifiers.select{|i| (i.class == CTSInventoryIdentifier)}.last
   end
-  
+
+  # split a urn identifier into its components
   def to_urn_components
     temp_components = self.to_components
     # should give us, e.g.
@@ -278,58 +413,6 @@ class CTSIdentifier < Identifier
     return urn_components
   end
   
-  def to_path
-    path_components = [ self.class::PATH_PREFIX ]
-    temp_components = self.to_components
-     # should give us, e.g.
-    # [0] collection = e.g. perseus
-    # [1] namespace - e.g. greekLang
-    # [2] work - e.g. tlg0012.tlg001 
-    # [3] edition or translation
-    # [4] perseus-grc1 - edition + examplar
-    # [5] 1.1 - passage
-    cts_inv = temp_components[0]
-    cts_ns = temp_components[1]
-    cts_urn = temp_components[2] + "." + temp_components[4]
-    cts_passage = temp_components[5]
-    
-    cts_textgroup,cts_work,cts_edition,cts_exemplar, =
-      cts_urn.split('.',4).collect {|x| x.tr(',/','-_')}
-    
-    # e.g. tlg0012.tlg001.perseus-grc1.1.1.xml
-    cts_xml_path_components = []
-    cts_xml_path_components << cts_textgroup
-    unless cts_work.nil?
-      cts_xml_path_components << cts_work
-    end
-    cts_xml_path_components << cts_edition 
-    unless cts_exemplar.nil? 
-      cts_xml_path_components << cts_exemplar 
-    end
-    unless cts_passage.nil? 
-      cts_xml_path_components << cts_passage 
-    end
-    cts_xml_path_components << 'xml' 
-    cts_xml_path = cts_xml_path_components.join('.')
-    
-    path_components << cts_inv
-    path_components << cts_ns
-    path_components << cts_textgroup
-    unless cts_work.nil?
-      path_components << cts_work
-    end
-    path_components << cts_xml_path
-    
-    # e.g. CTS_XML_PASSAGES/perseus/greekLang/tlg0012/tlg001/tlg0012.tlg001.perseus-grc1.1.1.xml
-    return File.join(path_components)
-  end
-  
-   ## get a link to the catalog for this identifier  
-  def get_catalog_link
-    #return "http://catalog.perseus.tufts.edu/perseus.org/xc/search/" + self.urn_attribute
-    return ''
-  end
-  
   # default xslt for displaying an annotation view of a CTS passage
   def passage_annotate_xslt
     File.read(File.join(Rails.root,%w{data xslt cts cts_annotate.xsl}))
@@ -338,89 +421,5 @@ class CTSIdentifier < Identifier
   # default xslt for retrieving the subref of a CTS passage
   def passage_subref_xslt_file
     File.join(Rails.root,%w{data xslt cts passage_to_subref.xsl})
-  end
-
-  def self.find_matching_identifiers(match_id,match_user,match_fuzzy)
-    identifiers = []
-    if (match_fuzzy)
-      possible_conflicts = Identifier.find_all_by_name(match_id, :include => :publication)
-      possible_conflicts = self.find(:all,
-                         :conditions => ["name like ?", "#{match_id}%"],
-                         :order => "name DESC")
-    else 
-      possible_conflicts = Identifier.find_all_by_name(match_id, :include => :publication)
-    end
-    actual_conflicts = possible_conflicts.select {|pc| ((pc.publication) && (pc.publication.owner == match_user) && !(%w{archived finalized}.include?(pc.publication.status)))}
-    identifiers += actual_conflicts
-    return identifiers
-  end
-
-  def download_file_name
-    urnObj = CTS::CTSLib.urnObj(self.urn_attribute)
-    file = urnObj.getTextGroup(false) + "." + urnObj.getWork(false) + "." + urnObj.getVersion(false)
-    begin
-      passage = urnObj.getPassage(100)
-      file = passage ? file + "." + passage : file 
-    rescue
-    end
-    file = file + ".xml"
-    file
-  end
-
-  # create a title for a publication of just this class identifier
-  def self.create_title(identifier_str)
-    identifier_str
-  end
-
-  # parse individual docs and metadata from a supplied TEI XML document
-  def self.parse_docs(content)
-  end  
-
-  def self.parse_content_for_identifier(a_content)
-    xml = REXML::Document.new(a_content).root
-    urn = REXML::XPath.first(xml,'/tei:TEI/tei:teiHeader/tei:fileDesc/tei:publicationStmt/tei:idno[@type="urn:cts"]',{"tei" => NS_TEI})
-    text = REXML::XPath.first(xml,'/tei:TEI/tei:text/tei:body/tei:div',{"tei" => NS_TEI})
-    unless urn.nil?
-      urn = urn.text
-    end
-    unless text.nil?
-      lang = text.attributes['xml:lang']    
-      pubtype = text.attributes['type']
-    end
-    unless (urn && lang && pubtype)
-      raise Exception.new("Unable to parse information for new URN identifier")
-    end
-    begin
-      urnObj = CTS::CTSLib.urnObj(urn)
-    rescue
-      raise Exception.new("Invalid URN identifier #{urn}")
-    end
-    # we must have at least a work
-    work = urnObj.getWork(false)
-    if (work.nil? || work == '')
-      raise Exception.new("Missing work identifier in #{urn}")
-    end
-    version = urnObj.getVersion(false)
-    if (version)
-      raise Exception.new("Creating a new version from a version URN is not yet supported")
-    end
-    self::next_temporary_identifier(self::TEMPORARY_COLLECTION,urn,pubtype,lang)
-  end
-      
-
-  def self.create_from_supplied(a_publication,a_agent,a_body,a_comment)
-    temp_id = self.new(:name => self.parse_content_for_identifier(a_body))
-    temp_id.publication = a_publication 
-    temp_id.save!
-    ## replace work urn with version 
-    urn = temp_id.urn_attribute
-    urnObj = CTS::CTSLib.urnObj(urn)
-    workUrn = "urn:cts:" + urnObj.getTextGroup(true) + "." + urnObj.getWork(false)
-    # TODO we should really only do this explicitly in the idno header
-    a_body.gsub!(/\b#{workUrn}\b/,temp_id.urn_attribute)
-    temp_id.set_content(a_body, :comment => a_comment, :actor => (a_publication.owner.class == User) ? a_publication.owner.jgit_actor : a_publication.creator.jgit_actor)
-    template_init = temp_id.add_change_desc(a_comment)
-    temp_id.set_xml_content(template_init, :comment => 'Initializing Content')
-    return temp_id
   end
 end

commit db547e9f6677e41dd726fbdca1a7ad36b4f48a1e

fix eagle mediawiki integration bug
Code was crashing because of a missing variable.

In `4dc207fc6f4c80f32372ca334736ef9252ad6b28`, code was copied and
pasted from another section and used a variable called`workUrn`. That
variable was not defined in the context it was used.

In addition, the source that was copied and pasted had a bug in it,
using `gsub` instead of `gsub!`. That bug was introduced in
`6d1ef15039ab6aea9588ec79936009de81de4e41`. This commit fixes both
issues and is another step towards fixing EAGLE integration.

app/models/cts_identifier.rb
@@ -46,7 +46,8 @@ class CTSIdentifier < Identifier
       raise Exception.new("Creating a new version from a version URN is not yet supported")
     end
     id = self::next_temporary_identifier(self::TEMPORARY_COLLECTION,urn,pubtype,lang)
-    content.gsub(/\b#{workUrn}\b/,id.urn_attribute)
+    workUrn = "urn:cts:" + urnObj.getTextGroup(true) + "." + urnObj.getWork(false)
+    content.gsub!(/\b#{workUrn}\b/,id.urn_attribute)
     return id,content
   end
 