7a0d27229d3ef08b23211e70b9d1328e631e2d6b
Ryan Baumann <ryan.baumann@gmail.com>
2021-02-16 11:19:21 -0500

    Add back database truncation on test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -62,6 +62,7 @@ class ActiveSupport::TestCase
   end
   
   def setup_database_cleaner
+    DatabaseCleaner.clean_with :truncation
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.start
   end
